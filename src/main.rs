c3365b3079622d445871a33bcb909cb15a0b6cb3
Vera Gonzalez <me@vera.lgbt>
2024-12-17 17:51:02 -0500

    added picker for device

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,74 @@
-use iced::widget::{column, pick_list, scrollable, vertical_space};
+use std::collections::HashMap;
+use std::fmt::Display;
+
+use iced::widget::{button, column, pick_list, scrollable, text, vertical_space};
 use iced::{Center, Element, Fill};
+use nokhwa::pixel_format::RgbFormat;
+use nokhwa::utils::{CameraIndex, CameraInfo, RequestedFormat, RequestedFormatType};
+use nokhwa::{native_api_backend, query, Camera};
 
 pub fn main() -> iced::Result {
-    iced::run("Pick List - Iced", Example::update, Example::view)
+    iced::run("Pick List - Iced", JustStop::update, JustStop::view)
 }
 
 #[derive(Default)]
-struct Example {
-    selected_language: Option<Language>,
+struct JustStop {
+    available_devices: Vec<String>,
+    available_indices: Vec<CameraIndex>,
+    selected_device: Option<String>,
+    camera: Option<Camera>,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum Message {
-    LanguageSelected(Language),
+    DeviceSelected(String),
+    RefreshDeviceList,
 }
 
-impl Example {
+impl JustStop {
     fn update(&mut self, message: Message) {
+        use Message::*;
         match message {
-            Message::LanguageSelected(language) => {
-                self.selected_language = Some(language);
+            DeviceSelected(device_name) => {
+                let index = self
+                    .available_devices
+                    .iter()
+                    .position(|name| *name == device_name)
+                    .unwrap()
+                    .clone();
+
+                let requested = RequestedFormat::new::<RgbFormat>(
+                    RequestedFormatType::AbsoluteHighestFrameRate,
+                );
+                self.camera =
+                    Some(Camera::new(self.available_indices[index].clone(), requested).unwrap());
+            }
+            RefreshDeviceList => {
+                let backend = native_api_backend().unwrap();
+                let devices = query(backend).unwrap();
+                self.available_devices = devices.iter().map(CameraInfo::human_name).collect();
+                self.available_indices = devices.iter().map(CameraInfo::index).cloned().collect();
             }
         }
     }
 
     fn view(&self) -> Element<Message> {
         let pick_list = pick_list(
-            &Language::ALL[..],
-            self.selected_language,
-            Message::LanguageSelected,
+            &self.available_devices[..],
+            self.selected_device.clone(),
+            Message::DeviceSelected,
         )
         .placeholder("Choose a language...");
+
+        let refresh_button = button(text("refresh list")).on_press(Message::RefreshDeviceList);
+
+        //let p2 = pick_list(options, selected, on_selected).placeholder("");
 
         let content = column![
             vertical_space().height(600),
             "Which is your favorite language?",
             pick_list,
+            refresh_button,
             vertical_space().height(600),
         ]
         .width(Fill)
@@ -43,47 +76,5 @@ impl Example {
         .spacing(10);
 
         scrollable(content).into()
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
-pub enum Language {
-    #[default]
-    Rust,
-    Elm,
-    Ruby,
-    Haskell,
-    C,
-    Javascript,
-    Other,
-}
-
-impl Language {
-    const ALL: [Language; 7] = [
-        Language::C,
-        Language::Elm,
-        Language::Ruby,
-        Language::Haskell,
-        Language::Rust,
-        Language::Javascript,
-        Language::Other,
-    ];
-}
-
-impl std::fmt::Display for Language {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Language::Rust => "Rust",
-                Language::Elm => "Elm",
-                Language::Ruby => "Ruby",
-                Language::Haskell => "Haskell",
-                Language::C => "C",
-                Language::Javascript => "Javascript",
-                Language::Other => "Some other language",
-            }
-        )
     }
 }
