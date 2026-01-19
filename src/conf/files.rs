7c40590c1f514fa3107aa84d002aa7d48d93d28c
Vera Gonzalez <me@vera.lgbt>
2026-01-18 22:54:22 -0500

    i am jesus

--- src/conf/files.rs
+++ src/conf/files.rs
@@ -36,8 +36,8 @@ impl FileManager {
         // Create a transparent image for the first snapshot
         FfmpegCommand::new()
             .format("lavfi")
-            // .input("color=c=pink:s=1920x1080,format=rgba,colorchannelmixer=aa=0.2")
-            .input("color=c=black:s=1920x1080,format=rgba,colorchannelmixer=aa=0.0")
+            .input("color=c=pink:s=1920x1080,format=rgba,colorchannelmixer=aa=0.2")
+            // .input("color=c=black:s=1920x1080,format=rgba,colorchannelmixer=aa=0.0")
             .frames(1)
             // .pix_fmt("bgra")
             .overwrite()
@@ -107,6 +107,7 @@ impl FileManager {
 
     pub fn snapshot(&self) -> String {
         self.tmp("snapshot.png")
+        // "output.png".to_string()
     }
 
     // pub fn output_is_ready(&self) -> bool {
@@ -357,33 +358,91 @@ impl FileManager {
         let mut command = FfmpegCommand::new();
         // ffmpeg -f v4l2 -i /dev/video3 -f rawvideo -pix_fmt yuv420p -y /tmp/output.pipe
         // ffmpeg -f v4l2 -i /dev/video3   -f mpegts   -codec:v libx264 -preset ultrafast -tune zerolatency   -g 1   -bf 0   -fflags nobuffer   -flush_packets 1   udp://127.0.0.1:8090?pkt_size=1316
+        // ffmpeg
+        //  -f v4l2
+        //  -i /dev/video3
+        //  -i ./photos/latest.png
+        //  -i ./photos/preview.png
+        //  -filter_complex "[0:v]split=2[snapshot][cam];[cam][2:v]overlay[overlay];[overlay][1:v]hstack[output];[snapshot]fps=1[snap1fps]"
+        //  -map [output]
+        //      -c:v libx264
+        //      -preset:v ultrafast
+        //      -tune zerolatency
+        //      -g 1
+        //      -bf 0
+        //      -fflags nobuffer
+        //      -flush_packets 1
+        //      -f mpegts
+        //      udp://127.0.0.1:8090?pkt_size=1316
+        //  -map [snap1fps]
+        //      -update 1
+        //      -y /tmp/just-stop/snapshot.png
+        //
+        // ffmpeg -f v4l2 -i /dev/video3 -i ./photos/latest.png -i ./photos/preview.png -filter_complex "[0:v]split=2[snapshot][cam];[cam][2:v]overlay[overlay];[overlay][1:v]hstack[output];[snapshot]fps=1[snap1fps]" -map [output] -c:v libx264 -preset:v ultrafast -tune zerolatency -g 1 -bf 0 -fflags nobuffer -flush_packets 1 -f mpegts udp://127.0.0.1:8090?pkt_size=1316 -map [snap1fps] -update 1 -y /tmp/just-stop/snapshot.png
+
+        //ffmpeg -re -f v4l2 -i /dev/video3 -loop 1 -i pink_overlay.png -filter_complex "[0:v][1:v]overlay=0:0,split=2[vpng][vudp];[vpng]fps=1[vpngout]" -map "[vpngout]" -f image2 -update 1 output.png -map "[vudp]" -c:v libx264 -preset veryfast -tune zerolatency -f mpegts udp://127.0.0.1:5000
+        // one more try
         command
+            .realtime()
             .format("v4l2")
             .input(&input)
-            // .args(["-loop", "1"])
-            // .input(&self.latest())
-            // .args(["-loop", "1"])
-            // .input(&self.preview())
-            // .filter_complex(effects.filter_complex(&self))
-            // .map("[output]")
-            // .format("rawvideo")
-            .format("mpegts")
+            .args(["-loop", "1"])
+            .input(&self.preview())
+            .filter_complex("[0:v][1:v]overlay=0:0,split=2[vpng][vudp];[vpng]fps=1[vpngout]")
+            .map("[vpngout]")
+            .format("image2")
+            .args(["-update", "1"])
+            .overwrite()
+            .output(self.snapshot())
+            .map("[vudp]")
             .codec_video("libx264")
-            .preset("ultrafast")
+            .preset("veryfast")
             .args(["-tune", "zerolatency"])
-            .args(["-g", "1"])
-            .args(["-bf", "0"])
-            .args(["-fflags", "nobuffer"])
-            .args(["-flush_packets", "1"])
-            // .pix_fmt("yuv420p")
-            // .overwrite()
-            .output(&format!("{}?pkt_size=1316", stream.to_string()))
-            // .map("[snapshot]")
-            // .rate(1.0)
-            // .args(["-update", "1"])
-            // .overwrite()
-            // .output(&self.snapshot())
+            .format("mpegts")
+            .output(&stream.to_string())
             .print_command();
+
+        // command
+        //     .format("v4l2")
+        //
+        //     .args(["-framerate", "60"])
+        //     .input(&input)
+        //     .input(&self.latest())
+        //     .input(&self.preview())
+        //     // .filter_complex("[0:v][2:v]overlay[mux];[mux][1:v]hstack[output]")
+        //     .filter_complex("[0:v]split=2[snapshot][cam];[cam][2:v]overlay[overlay];[overlay][1:v]hstack[output];[snapshot]fps=1[snap1fps]")
+        //     // .filter_complex("[0:v]split=2[snapshot][cam];[snapshot]fps=1[snapped];[cam][2:v]overlay[overlay];[overlay][1:v]hstack[output]")
+        //     // .args(["-loop", "1"])
+        //     // .input(&self.preview())
+        //     // .filter_complex(effects.filter_complex(&self))
+        //     .map("[output]")
+        //     // .format("rawvideo")
+        //     // .args(["-c:v:0", "libx264"])
+        //     .codec_video("libx264")
+        //             // .args(["-x264-params", "repeat-headers=1:bframes=0"])
+        //
+        //     // .args(["-preset:v:0", "ultrafast"])
+        //     .preset("ultrafast")
+        //     .args(["-tune", "zerolatency"])
+        //     .args(["-g", "1"])
+        //     .args(["-bf", "0"])
+        //     .args(["-fflags", "nobuffer"])
+        //     .args(["-flush_packets", "1"])
+        //     // .format("mpegts")
+        //     // .output(&format!("{}?pkt_size=1316", stream.to_string()))
+        //     .args(["-f", "mpegts", &format!("{}?pkt_size=1316", stream.to_string())])
+        //     // .pix_fmt("yuv420p")
+        //     // .overwrite()
+        //     // .output(&format!("{}?pkt_size=1316", stream.to_string()))
+        //     .map("[snap1fps]")
+        //     // .args(["-c", "png"])
+        //     // .codec_video("png")
+        //     // .args(["-r:v:1", "1"])
+        //     .args(["-update", "1"])
+        //     .args(["-y", &self.snapshot()])
+        //     // .overwrite()
+        //     // .output(&self.snapshot())
+        //     .print_command();
         /* command
         .format("v4l2")
         // .realtime()
--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,21 @@ use ffmpeg_sidecar::{
     event::{FfmpegEvent, LogLevel},
 };
 use inquire::Select;
-use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::thread;
 use std::time::Duration;
 use std::{
     fs::{self, File},
     io::Write,
 };
+use std::{io::BufRead as _, thread};
+use std::{
+    io::BufReader,
+    sync::atomic::{AtomicBool, Ordering},
+};
 use std::{
     os::unix::fs::FileTypeExt,
     path::{Path, PathBuf},
 };
+use std::{process::Stdio, sync::Arc};
 use std::{
     process::{Child, Command},
     thread::sleep,
@@ -255,16 +258,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                             // ffplay -f rawvideo -pixel_format yuv420p -video_size 1920x1080 -framerate 60 /tmp/output.pipe
                             let mut ffplay_cmd = Command::new("ffplay");
                             ffplay_cmd
+                                // .args(["-f", "mpegts"])
+                                // .args(["-framerate", "60"])
                                 .args(["-fflags", "nobuffer"])
                                 .args(["-flags", "low_delay"])
-                                .arg("-framedrop")
-                                .args(["-probesize", "32"])
-                                .args(["-analyzeduration", "0"])
+                                // .arg("-framedrop")
+                                // .args(["-probesize", "32"])
+                                // // .args(["-"])
+                                // .args(["-analyzeduration", "0"])
+                                .stdout(Stdio::piped())
+                                .stderr(Stdio::piped())
                                 // .args(["-f", "rawvideo"])
                                 //
                                 // .args(["-pixel_format", "yuv420p"])
                                 // .args(["-video_size", "1920x1080"])
-                                // .args(["-framerate", "60"])
                                 // .arg(file_manager.output_socket());
                                 .arg(&output_path);
 
@@ -272,9 +279,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                             if ffplay_pid.is_some() {
                                 println!("already healthy");
-                            } else if let Ok(child) = ffplay_cmd.spawn() {
-                                println!("spawned ffplay");
+                            } else if let Ok(mut child) = ffplay_cmd.spawn() {
                                 ffplay_pid = Some(child.id());
+                                println!("spawned ffplay: ${ffplay_pid:?}");
+
+                                // Take ownership of stdout and stderr
+                                let stdout = child.stdout.take().unwrap();
+                                let stderr = child.stderr.take().unwrap();
+
+                                // Spawn thread for stdout logging
+                                std::thread::spawn(move || {
+                                    let reader = BufReader::new(stdout);
+                                    for line in reader.lines() {
+                                        if let Ok(line) = line {
+                                            println!("ffplay info: {}", line);
+                                        }
+                                    }
+                                });
+
+                                // Spawn thread for stderr logging
+                                std::thread::spawn(move || {
+                                    let reader = BufReader::new(stderr);
+                                    for line in reader.lines() {
+                                        if let Ok(line) = line {
+                                            eprintln!("ffplay error: {}", line);
+                                        }
+                                    }
+                                });
                             } else {
                                 println!("error occurred spawning ffplay");
                             }
@@ -318,6 +349,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //     println!("waiting for preview file to be generated");
     //     sleep(Duration::from_millis(333));
     // }
+    // s.send(Message::Start)?;
 
     // thread::sleep(Duration::from_secs(5));
     // // Start the streaming loop
@@ -390,25 +422,25 @@ fn stream(
 
         match event {
             FfmpegEvent::Log(LogLevel::Info, msg) => {
-                println!("FFmpeg info: {}", msg);
+                println!("ffmpeg info: {}", msg);
             }
             FfmpegEvent::Log(LogLevel::Warning, msg) => {
-                println!("FFmpeg warning: {}", msg);
+                println!("ffmpeg warning: {}", msg);
             }
             FfmpegEvent::Log(LogLevel::Error, msg) => {
-                eprintln!("FFmpeg error: {}", msg);
+                eprintln!("ffmpeg error: {}", msg);
             }
             FfmpegEvent::Progress(progress) => {
                 if progress.frame % 30 == 0 {
                     // Log every second at 30fps
                     println!(
-                        "Processed {} frames, time: {:.2}s",
+                        "ffmpeg Processed {} frames, time: {:.2}s",
                         progress.frame, progress.time
                     );
                 }
             }
             FfmpegEvent::LogEOF => {
-                println!("FFmpeg log ended");
+                println!("ffmpeg log ended");
                 break;
             }
             _ => {}
